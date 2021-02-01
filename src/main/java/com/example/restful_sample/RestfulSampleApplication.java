ba9738df3e7f63ff301cabe7557e8782f5db0757
Real-Bae-tory <dunkey2020@gmail.com>
2021-02-01 23:51:56 +0900

    추가 : 프로젝트 생성

---
+++ src/main/java/com/example/restful_sample/RestfulSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.restful_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulSampleApplication.class, args);
+	}
+
+}
