a75c4266b4231fad42ed3e80ab9fa36dea0c5642
Real-Bae-tory <dunkey2020@gmail.com>
2021-02-02 00:33:15 +0900

    추가 : User DTO 필드

--- src/main/java/com/example/restful_sample/user/User.java
+++ src/main/java/com/example/restful_sample/user/User.java
@@ -1,4 +1,18 @@
 package com.example.restful_sample.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+
+    private Integer id;
+    private String name;
+    private Date joinDate;
+
 }
