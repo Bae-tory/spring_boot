abfb563340cae5994a58090f778247ebc341e0a0
Real-Bae-tory <dunkey2020@gmail.com>
2021-02-02 00:38:55 +0900

    추가 : UserDaoService 작성 - findAll,findOne,save

--- src/main/java/com/example/restful_sample/user/UserDaoService.java
+++ src/main/java/com/example/restful_sample/user/UserDaoService.java
@@ -1,6 +1,40 @@
 package com.example.restful_sample.user;
 
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class UserDaoService {
+
+    private static List<User> usersList = new ArrayList<>();
+    private static int usersCount = 3;
+
+    static {
+        usersList.add(new User(1, "홍길이", new Date()));
+        usersList.add(new User(2, "순딩이", new Date()));
+        usersList.add(new User(3, "봉만이", new Date()));
+    }
+
+    public List<User> findAll() {
+        return usersList;
+    }
+
+    public User save(User user) {
+        if (user.getId() == null) {
+            user.setId(++usersCount);
+        }
+        usersList.add(user);
+        return user;
+    }
+
+    public User findOne(int id) {
+        for (User user : usersList) {
+            if (user.getId() == id) {
+                return user;
+            }
+        }
+        return null;
+    }
 
 }
