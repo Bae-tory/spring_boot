39fe6aae6c1b73a4172edc3b2028278042fe670e
Real-Bae-tory <dunkey2020@gmail.com>
2021-02-02 01:00:43 +0900

    수정 : @POST 유저 생성 201 created 반환코드

--- src/main/java/com/example/restful_sample/user/UserController.java
+++ src/main/java/com/example/restful_sample/user/UserController.java
@@ -1,7 +1,10 @@
 package com.example.restful_sample.user;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -26,8 +29,13 @@ public class UserController {
     }
 
     @PostMapping("/user")
-    public void createUser(@RequestBody User user) {
+    public ResponseEntity<User> createUser(@RequestBody User user) {
         User newUser = userDaoService.save(user);
 
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(newUser.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
     }
 }
