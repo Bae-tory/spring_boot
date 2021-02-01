4ec0ada3b5e0692d1c24695ea4feea38e71d120f
Real-Bae-tory <dunkey2020@gmail.com>
2021-02-02 01:14:44 +0900

    추가 : Exception 핸들러 클래스

---
+++ src/main/java/com/example/restful_sample/exception/CustomResponseEntityExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.example.restful_sample.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@RestController
+@ControllerAdvice // 모든 컨트롤러가 실행될 때 이 bean이 실행되게끔 함
+
+public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler { // 모든 예외상황을 핸들링 할 수 있는 클래스
+
+    @ExceptionHandler // 이 메서드가 익셉션 핸들러로 쓰이게
+    public final ResponseEntity<Object> handleAllExceptions(Exception e, WebRequest request) {
+        ExceptionResponse exceptionResponse =
+                new ExceptionResponse(new Date(), e.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
