5a85d82fc44b1c4ac5a7abe9361715d484eaacc9
Real-Bae-tory <dunkey2020@gmail.com>
2021-02-02 01:18:51 +0900

    리팩토링 : UserNotFoundException 용 핸들러

--- src/main/java/com/example/restful_sample/exception/CustomResponseEntityExceptionHandler.java
+++ src/main/java/com/example/restful_sample/exception/CustomResponseEntityExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.restful_sample.exception;
 
+import com.example.restful_sample.user.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,11 +16,20 @@ import java.util.Date;
 
 public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler { // 모든 예외상황을 핸들링 할 수 있는 클래스
 
-    @ExceptionHandler // 이 메서드가 익셉션 핸들러로 쓰이게
+    @ExceptionHandler(Exception.class) // 이 메서드가 익셉션 핸들러로 쓰이게(일반 에러)
     public final ResponseEntity<Object> handleAllExceptions(Exception e, WebRequest request) {
         ExceptionResponse exceptionResponse =
                 new ExceptionResponse(new Date(), e.getMessage(), request.getDescription(false));
 
         return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
+    @ExceptionHandler(UserNotFoundException.class) // UserNotFoundException이 일어나면 해당 메서드가 실행됨
+    public final ResponseEntity<Object> handleUserNotFoundException(Exception e, WebRequest request) {
+        ExceptionResponse exceptionResponse =
+                new ExceptionResponse(new Date(), e.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+
 }
