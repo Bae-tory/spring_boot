02e54647e3602deaa6be9f6ddabb10455585d1cc
Real-Bae-tory <dunkey2020@gmail.com>
2021-02-02 00:54:33 +0900

    추가 : @POST 유저 생성

--- src/main/java/com/example/restful_sample/user/UserController.java
+++ src/main/java/com/example/restful_sample/user/UserController.java
@@ -1,8 +1,6 @@
 package com.example.restful_sample.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,4 +25,9 @@ public class UserController {
         return userDaoService.findOne(id);
     }
 
+    @PostMapping("/user")
+    public void createUser(@RequestBody User user) {
+        User newUser = userDaoService.save(user);
+
+    }
 }
