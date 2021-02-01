4309837edcbe80539a90532bc4281eb9c56bf4f6
Real-Bae-tory <dunkey2020@gmail.com>
2021-02-02 00:41:59 +0900

    추가 : UserController 작성, UserDaoService annotation

--- src/main/java/com/example/restful_sample/user/UserController.java
+++ src/main/java/com/example/restful_sample/user/UserController.java
@@ -1,4 +1,15 @@
 package com.example.restful_sample.user;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+    // 의존성 주입
+    private UserDaoService userDaoService;
+
+    public UserController(UserDaoService userDaoService) {
+        this.userDaoService = userDaoService;
+    }
+
 }
--- src/main/java/com/example/restful_sample/user/UserDaoService.java
+++ src/main/java/com/example/restful_sample/user/UserDaoService.java
@@ -1,10 +1,13 @@
 package com.example.restful_sample.user;
 
 
+import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+@Service
 public class UserDaoService {
 
     private static List<User> usersList = new ArrayList<>();
