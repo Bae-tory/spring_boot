687f9a7a46c39aeb55a3c52fe075097c882b9a38
Real-Bae-tory <dunkey2020@gmail.com>
2021-02-02 23:07:53 +0900

    추가 : 다국어를 위한 기반코드

--- src/main/java/com/example/restful_sample/RestfulSampleApplication.java
+++ src/main/java/com/example/restful_sample/RestfulSampleApplication.java
@@ -2,12 +2,25 @@ package com.example.restful_sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class RestfulSampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestfulSampleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulSampleApplication.class, args);
+    }
 
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.KOREA);
+        return localeResolver;
+    }
 }
+
