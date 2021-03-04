878a06dd6527e0ed025425b4d0f81dc6675b9887
Real-Bae-tory <dunkey2020@gmail.com>
2021-03-04 20:23:58 +0900

    추가 : API 버전관리

--- src/main/java/com/example/restful_sample/user/UserAdminController.java
+++ src/main/java/com/example/restful_sample/user/UserAdminController.java
@@ -3,6 +3,7 @@ package com.example.restful_sample.user;
 import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.beans.BeanUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
@@ -38,9 +39,12 @@ public class UserAdminController {
         return mappingJacksonValue;
     }
 
-    // GET /user/1 or /user/2 -> String이지만 int로 자동 맵핑됨
-    @GetMapping("/user/{id}")
-    public MappingJacksonValue/*filter에 걸리기 위해*/ getUser(@PathVariable int id) {
+    //     GET /user/1 or /user/2 -> String이지만 int로 자동 맵핑됨  -> /admin/v1/users/1
+//    @GetMapping(value = "/user/{id}", params = "version=1")
+//    @GetMapping("/v1/user/{id}")
+//    @GetMapping(value = "/user/{id}", headers = "API_VERSION=1")
+    @GetMapping(value = "/user/{id}", produces = "application/vnd.company.appv1+json")
+    public MappingJacksonValue/*filter에 걸리기 위해*/ getUserV1(@PathVariable int id) {
         User userById = userDaoService.findOne(id);
         if (userById == null) {
             throw new UserNotFoundException(String.format(("ID[%s] not found"), id));
@@ -52,6 +56,32 @@ public class UserAdminController {
         FilterProvider filterProvider = new SimpleFilterProvider().addFilter("UserInfo", filter);
 
         MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(userById);
+        mappingJacksonValue.setFilters(filterProvider);
+
+        return mappingJacksonValue;
+    }
+
+    //    @GetMapping(value = "/user/{id}", params = "version=1")
+//    @GetMapping("/v2/user/{id}")
+//    @GetMapping(value = "/user/{id}", headers = "API_VERSION=2")
+    @GetMapping(value = "/user/{id}", produces = "application/vnd.company.appv2+json")
+    public MappingJacksonValue/*filter에 걸리기 위해*/ getUserV2(@PathVariable int id) {
+        User userById = userDaoService.findOne(id);
+        if (userById == null) {
+            throw new UserNotFoundException(String.format(("ID[%s] not found"), id));
+        }
+
+        // User를 UserV2로 복사
+        UserV2 userV2 = new UserV2();
+        BeanUtils.copyProperties(userById, userV2); // id, name, joinDate, password, ssn
+        userV2.setGrade("VIP");
+
+        SimpleBeanPropertyFilter filterV2 = SimpleBeanPropertyFilter
+                .filterOutAllExcept("id", "name", "joinDate", "grade");
+
+        FilterProvider filterProvider = new SimpleFilterProvider().addFilter("UserInfoV2", filterV2);
+
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(userV2);
         mappingJacksonValue.setFilters(filterProvider);
 
         return mappingJacksonValue;
