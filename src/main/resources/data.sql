f4fc1eca4872766df4f95287f55b8ea0cf1e57ab
Real-Bae-tory <dunkey2020@gmail.com>
2021-03-06 13:12:02 +0900

    추가: data.sql post 초기데이터

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,12 +5,20 @@ insert into user values(400, sysDate(), 'User4','test1234','920923-1010104');
 insert into user values(500, sysDate(), 'User5','test1234','920923-1010105');
 
 insert into user (id, join_date, name, password, ssn)
-VALUES (600, sysDate(), 'User6','test1234','920923-1010106');
+values (600, sysDate(), 'User6','test1234','920923-1010106');
 insert into user (id, join_date, name, password, ssn)
-VALUES (700, sysDate(), 'User7','test1234','920923-1010107');
+values (700, sysDate(), 'User7','test1234','920923-1010107');
 insert into user (id, join_date, name, password, ssn)
-VALUES (800, sysDate(), 'User8','test1234','920923-1010108');
+values (800, sysDate(), 'User8','test1234','920923-1010108');
 insert into user (id, join_date, name, password, ssn)
-VALUES (900, sysDate(), 'User9','test1234','920923-1010109');
+values (900, sysDate(), 'User9','test1234','920923-1010109');
 
-
+insert into post values(1,sysDate(),'내용입니다',100);
+insert into post values(2,sysDate(),'내용입니다',100);
+insert into post values(3,sysDate(),'내용입니다',100);
+insert into post values(4,sysDate(),'내용입니다',200);
+insert into post values(5,sysDate(),'내용입니다',200);
+insert into post values(6,sysDate(),'내용입니다',200);
+insert into post values(7,sysDate(),'내용입니다',500);
+insert into post values(8,sysDate(),'내용입니다',500);
+insert into post values(9,sysDate(),'내용입니다',700);
