4e6ebbfe9782bf9495750db825f602d5ac2313d0
Real-Bae-tory <dunkey2020@gmail.com>
2021-03-06 12:02:24 +0900

    수정 : data.sql insert 2가지 방식

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,14 @@
-insert into user values(1, sysDate(), "User1","test1234","920923-1010101");
-insert into user values(2, sysDate(), "User2","test1234","920923-1010102");
-insert into user values(3, sysDate(), "User3","test1234","920923-1010103");
-insert into user values(4, sysDate(), "User4","test1234","920923-1010104");
-insert into user values(5, sysDate(), "User5","test1234","920923-1010105");
-insert into user values(6, sysDate(), "User6","test1234","920923-1010106");
+insert into user values(1, sysDate(), 'User1','test1234','920923-1010101');
+insert into user values(2, sysDate(), 'User2','test1234','920923-1010102');
+insert into user values(3, sysDate(), 'User3','test1234','920923-1010103');
+insert into user values(4, sysDate(), 'User4','test1234','920923-1010104');
+insert into user values(5, sysDate(), 'User5','test1234','920923-1010105');
+
+insert into user (id, join_date, name, password, ssn)
+VALUES (6, sysDate(), 'User6','test1234','920923-1010106');
+insert into user (id, join_date, name, password, ssn)
+VALUES (7, sysDate(), 'User7','test1234','920923-1010107');
+insert into user (id, join_date, name, password, ssn)
+VALUES (8, sysDate(), 'User8','test1234','920923-1010108');
+insert into user (id, join_date, name, password, ssn)
+VALUES (9, sysDate(), 'User9','test1234','920923-1010109');
