893e6f94b9d77bf2f62e52e7405ead181ad039b5
Real-Bae-tory <dunkey2020@gmail.com>
2021-03-06 11:16:24 +0900

    리팩토링 : User -> Entity 선언

--- src/main/java/com/example/restful_sample/user/User.java
+++ src/main/java/com/example/restful_sample/user/User.java
@@ -6,6 +6,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
@@ -16,8 +19,11 @@ import java.util.Date;
 //@JsonIgnoreProperties(value = "password")
 //@JsonFilter("UserInfo")
 @ApiModel(description = "사용자 상세 정보를 위한 도메인 객체")
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue
     private Integer id;
     @Size(min = 2, message = "이름은 2자 이상이어야 합니다.")
     @ApiModelProperty(notes = "사용자 이름을 입력해 주세요.")
@@ -31,3 +37,15 @@ public class User {
     private String ssn;
 
 }
+
+/**
+ * Entity = Create Table
+ * create table user (
+ * id integer not null,
+ * join_date timestamp,
+ * name varchar(255),
+ * password varchar(255),
+ * ssn varchar(20),
+ * primary key (id)
+ * )
+ */
