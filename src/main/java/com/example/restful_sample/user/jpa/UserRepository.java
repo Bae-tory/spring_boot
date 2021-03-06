791711a15e523c45ce64a3eb98ae0fbe5c98b0c3
Real-Bae-tory <dunkey2020@gmail.com>
2021-03-06 12:02:47 +0900

    추가 : UserRepository

---
+++ src/main/java/com/example/restful_sample/user/jpa/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.restful_sample.user.jpa;
+
+import com.example.restful_sample.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository // 데이터베이스 관련된 형태의 Bean
+public interface UserRepository extends JpaRepository<User,Integer> {
+
+
+}
