82bef0b28f00780ae8657fcaac634c1399e5bb53
Real-Bae-tory <dunkey2020@gmail.com>
2021-03-06 12:33:11 +0900

    추가 : JPA로 유저 생성 로직

--- src/main/java/com/example/restful_sample/user/jpa/UserJpaController.java
+++ src/main/java/com/example/restful_sample/user/jpa/UserJpaController.java
@@ -7,7 +7,9 @@ import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -34,10 +36,10 @@ public class UserJpaController {
     }
 
     @GetMapping("/user/link/{id}")
-    public ResponseEntity<EntityModel<User>> getUserEntity(@PathVariable int id){
+    public ResponseEntity<EntityModel<User>> getUserEntity(@PathVariable int id) {
         Optional<User> user = userRepository.findById(id);
 
-        if(!user.isPresent()){ // 존재하지 않는다면
+        if (!user.isPresent()) { // 존재하지 않는다면
             throw new UserNotFoundException(String.format("ID[%s] not found", id));
         }
 
@@ -52,7 +54,19 @@ public class UserJpaController {
     }
 
     @DeleteMapping("/user/{id}")
-    public void deleteUser(@PathVariable int id){
+    public void deleteUser(@PathVariable int id) {
         userRepository.deleteById(id);
+    }
+
+    @PostMapping("/user")
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+        User savedUser = userRepository.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest() // 저장된 user의 id값을 uri로 변경 -> Header에 Location Key로 전달
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
